cdc72614028be1332c0d7434899932d146b40562
camerontorres <camerontorres759@gmail.com>
2023-04-27 19:02:04 -0400

    first commit  with '#' will be ignored, and an empty message aborts the commit.

---
+++ routes/main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const homeController = require("../controllers/home");
+
+
+//Main Routes - simplified for now
+router.get("/", homeController.getIndex);
+router.get("/info", homeController.getInfo);
+
+
+
+module.exports = router;
